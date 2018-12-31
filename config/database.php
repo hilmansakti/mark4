101de3c33a8379b225f6edccd1c2d91cc8f2d6dc
Hilman Sakti <hilman.sakti22@gmail.com>
2018-12-31 11:00:12 +0700

    comm

--- config/database.php
+++ config/database.php
@@ -55,10 +55,10 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => '127.0.0.1',
-            'port'      => '3306',
+            'port'      => '8889',
             'database'  => 'mark4',
             'username'  => 'root',
-            'password'  => 'VitaminOrange',
+            'password'  => 'root',
             'charset'   => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix'    => '',
